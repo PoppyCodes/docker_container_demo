8030c20333c4fa036ba36213c35432e2c3c58e15
charles.pinart@gmail.com <ubuntu@ip-172-31-23-18.eu-west-1.compute.internal>
2023-11-16 18:02:16 +0000

    several dockerfile

---
+++ authent/test_authent.py
@@ -0,0 +1,137 @@
+import os
+import requests
+
+# définition de l'adresse de l'API
+api_address = "fastapi"
+#local
+#api_address = "localhost"
+# port de l'API
+api_port = 8000
+#log_file="logs/log_test_authent.log"
+log_file="./logs/log_test.log"
+
+# requête authent test 1: Alice avec des droits renvoie 200 SUCCESS
+r = requests.get(
+    url='http://{address}:{port}/permissions'.format(address=api_address, port=api_port),
+    params= {
+        'username': 'alice',
+        'password': 'wonderland'
+    }
+)
+
+output = '''
+============================
+    Authentication test 1
+============================
+
+request done at "/permissions"
+| username="alice"
+| password="wonderland"
+
+expected result = 200
+actual result = {status_code}
+
+==>  {test_status}
+
+'''
+
+
+# statut de la requête
+status_code = r.status_code
+
+# affichage des résultats
+if status_code == 200:
+    test_status = 'SUCCESS'
+else:
+    test_status = 'FAILURE'
+print(output.format(status_code=status_code, test_status=test_status))
+
+# impression dans un fichier
+if os.environ.get('LOG') == "1":
+    print("je lgo")
+    with open(log_file, 'a') as file:
+        file.write(output.format(status_code=status_code, test_status=test_status))
+else:
+    print("je log pas")
+
+# requête authent test 2: Bob avec des droits renvoie 200 SUCCESS
+r2 = requests.get(
+    url='http://{address}:{port}/permissions'.format(address=api_address, port=api_port),
+    params= {
+        'username': 'bob',
+        'password': 'builder'
+    }
+)
+
+output = '''
+============================
+    Authentication test 2
+============================
+
+request done at "/permissions"
+| username="bob"
+| password="builder"
+
+expected result = 200
+actual result = {status_code}
+
+==>  {test_status}
+
+'''
+
+
+# statut de la requête
+status_code = r2.status_code
+
+# affichage des résultats
+if status_code == 200:
+    test_status = 'SUCCESS'
+else:
+    test_status = 'FAILURE'
+print(output.format(status_code=status_code, test_status=test_status))
+
+# impression dans un fichier
+if os.environ.get('LOG') == "1":
+    with open(log_file, 'a') as file:
+        file.write(output.format(status_code=status_code, test_status=test_status))
+
+# requête authent test 3: user factice sans droits renvoie 403 Forbidden
+r3 = requests.get(
+    url='http://{address}:{port}/permissions'.format(address=api_address, port=api_port),
+    params= {
+        'username': 'clementine',
+        'password': 'mandarine'
+    }
+)
+
+output = '''
+============================
+    Authentication test 3
+============================
+
+request done at "/permissions"
+| username="clementine"
+| password="mandarine"
+
+expected result = 403
+actual result = {status_code}
+
+==>  {test_status}
+
+'''
+
+
+# statut de la requête
+status_code = r3.status_code
+
+# affichage des résultats
+if status_code == 403:
+    test_status = 'SUCCESS'
+else:
+    test_status = 'FAILURE'
+print(output.format(status_code=status_code, test_status=test_status))
+
+# impression dans un fichier
+if os.environ.get('LOG') == "1":
+    with open(log_file, 'a') as file:
+        file.write(output.format(status_code=status_code, test_status=test_status))
---
+++ authorization/test_authorization.py
@@ -0,0 +1,180 @@
+import os
+import requests
+
+# définition de l'adresse de l'API
+api_address = "fastapi"
+# port de l'API
+api_port = 8000
+log_file="./logs/log_test.log"
+
+# requête sentiment: user has rights on v1
+r = requests.get(
+    url='http://{address}:{port}/v1/sentiment'.format(address=api_address, port=api_port),
+    params= {
+        'username': 'alice',
+        'password': 'wonderland',
+	'sentence':'life is good'
+    }
+)
+
+output = '''
+============================
+    Authorization test 1: user has rights
+============================
+
+request done at "/v1/sentiment"
+| username="alice"
+| password="wonderland"
+| sentence="life is good"
+
+expected result = 200
+actual restult = {status_code}
+
+==>  {test_status}
+
+'''
+
+
+# statut de la requête
+status_code = r.status_code
+
+# affichage des résultats
+if status_code == 200:
+    test_status = 'SUCCESS'
+else:
+    test_status = 'FAILURE'
+print(output.format(status_code=status_code, test_status=test_status))
+
+# impression dans un fichier
+if int(os.environ.get('LOG')) == 1:
+    with open(log_file, 'a') as file:
+        file.write(output.format(status_code=status_code, test_status=test_status))
+
+
+# requête sentiment: user bob has rights on v1
+r = requests.get(
+    url='http://{address}:{port}/v1/sentiment'.format(address=api_address, port=api_port),
+    params= {
+        'username': 'bob',
+        'password': 'builder',
+	'sentence':'life is good'
+    }
+)
+
+output = '''
+============================
+    Authorization test 2: user has rights
+============================
+
+request done at "/v1/sentiment"
+| username="bob"
+| password="builder"
+| sentence="life is good"
+
+expected result = 200
+actual restult = {status_code}
+
+==>  {test_status}
+
+'''
+
+
+# statut de la requête
+status_code = r.status_code
+
+# affichage des résultats
+if status_code == 200:
+    test_status = 'SUCCESS'
+else:
+    test_status = 'FAILURE'
+print(output.format(status_code=status_code, test_status=test_status))
+
+# impression dans un fichier
+if int(os.environ.get('LOG')) == 1:
+    with open(log_file, 'a') as file:
+        file.write(output.format(status_code=status_code, test_status=test_status))
+
+# requête sentiment 3: user Alice has rights on v2
+r = requests.get(
+    url='http://{address}:{port}/v2/sentiment'.format(address=api_address, port=api_port),
+    params= {
+        'username': 'alice',
+        'password': 'wonderland',
+	'sentence':'life is good'
+    }
+)
+
+output = '''
+============================
+    Authorization test 3: user has rights on V2
+============================
+
+request done at "/v2/sentiment"
+| username="alice"
+| password="wonderland"
+| sentence="life is good"
+
+expected result = 200
+actual restult = {status_code}
+
+==>  {test_status}
+
+'''
+
+
+# statut de la requête
+status_code = r.status_code
+
+# affichage des résultats
+if status_code == 200:
+    test_status = 'SUCCESS'
+else:
+    test_status = 'FAILURE'
+print(output.format(status_code=status_code, test_status=test_status))
+
+# impression dans un fichier
+if int(os.environ.get('LOG')) == 1:
+    with open(log_file, 'a') as file:
+        file.write(output.format(status_code=status_code, test_status=test_status))
+
+# requête 4: user bob has no rights on V2
+r = requests.get(
+    url='http://{address}:{port}/v2/sentiment'.format(address=api_address, port=api_port),
+    params= {
+        'username': 'bob',
+        'password': 'builder'
+    }
+)
+
+output = '''
+============================
+    Authorization test 4: user has no rights
+============================
+
+request done at "/v2/sentiment"
+| username="bob"
+| password="builder"
+| sentence="life is good"
+
+expected result = 403
+actual restult = {status_code}
+
+==>  {test_status}
+
+'''
+
+
+# statut de la requête
+status_code = r.status_code
+
+# affichage des résultats
+if status_code == 403:
+    test_status = 'SUCCESS'
+else:
+    test_status = 'FAILURE'
+print(output.format(status_code=status_code, test_status=test_status))
+
+# impression dans un fichier
+if int(os.environ.get('LOG')) == 1:
+    with open(log_file, 'a') as file:
+        file.write(output.format(status_code=status_code, test_status=test_status))
